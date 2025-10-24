d144ea90dd2cfdd061496bff9e09bddf3706e2e2
JacobMuli <jacobmwalughs@gmail.com>
2025-10-24 17:15:59 +0300

    added preview config for vite render deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    preview: {
+        host: "0.0.0.0",
+        port: 10000,
+        allowedHosts: ["chirper-lsd5.onrender.com"],
+    },
 });
